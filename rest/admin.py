4d482f022d6c5bd888df7e2c8a5ac4d99bf9fa0e
Daniel Sarmiento <dsarmientos@gmail.com>
2011-12-03 19:57:45 -0500

    Todos los modelos en admin

--- rest/admin.py
+++ rest/admin.py
@@ -1,9 +1,12 @@
 # vim: set fileencoding=utf-8
 
 from django.contrib import admin
-from rest.models import Cliente, Ciudad, Servicio, Taller
+from rest.models import (Cliente, Ciudad, Cita, Servicio, Repuesto,
+                         TipoTransporte, Consumible, Evidencia,
+                         Mantenimiento, Vehiculo, Taller)
 
-models = [Cliente, Ciudad, Servicio, Taller]
+models = [Cliente, Ciudad, Cita, Servicio, Repuesto, TipoTransporte,
+          Consumible, Evidencia, Mantenimiento, Vehiculo, Taller]
 
 for model in models:
     admin.site.register(model)
