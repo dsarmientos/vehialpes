730e08927675c7e05d911b179d7e9bad6fa68321
Daniel Sarmiento <dsarmientos@gmail.com>
2011-12-04 16:40:42 -0500

    Pico y placa. Fecha compra vehiculo

--- rest/admin.py
+++ rest/admin.py
@@ -3,10 +3,10 @@
 from django.contrib import admin
 from rest.models import (Cliente, Ciudad, Cita, Servicio, Repuesto,
                          TipoTransporte, Consumible, Evidencia,
-                         Mantenimiento, Vehiculo, Taller)
+                         Mantenimiento, Vehiculo, Taller, PicoYPlaca)
 
 models = [Cliente, Ciudad, Cita, Servicio, Repuesto, TipoTransporte,
-          Consumible, Evidencia, Mantenimiento, Vehiculo, Taller]
+          Consumible, Evidencia, Mantenimiento, Vehiculo, Taller, PicoYPlaca]
 
 for model in models:
     admin.site.register(model)
--- rest/models.py
+++ rest/models.py
@@ -63,6 +63,29 @@ class Mantenimiento(models.Model):
     def __unicode__(self):
         return '-'.join((unicode(self.taller.nombre), self.vehiculo.placa))
 
+class PicoYPlaca(models.Model):
+    DIA_LUNES = 'L'
+    DIA_MARTES = 'M'
+    DIA_MIERCOLES = 'X'
+    DIA_JUEVES = 'J'
+    DIA_VIERNES = 'V'
+    DIA_SABADO = 'S'
+    DIA_DOMINGO = 'D'
+    DIA_CHOICES = (
+        (DIA_LUNES, 'Lunes'),
+        (DIA_MARTES, 'Martes'),
+        (DIA_MIERCOLES, 'Miercoles'),
+        (DIA_JUEVES, 'Jueves'),
+        (DIA_VIERNES, 'Viernes'),
+        (DIA_SABADO, 'Sabado'),
+        (DIA_DOMINGO, 'Domingo'),
+    )
+    ultimo_digito = models.IntegerField(unique=True)
+    dia_semana = models.CharField(choices=DIA_CHOICES, max_length='1')
+
+    def __unicode__(self):
+        return '-'.join((self.ultimo_digito, self.dia_semana))
+
 class Servicio(models.Model):
     nombre = models.CharField(max_length=200)
     descripcion = models.TextField(blank=True)
@@ -93,6 +116,7 @@ class Vehiculo(models.Model):
     tipo_caja = models.CharField(max_length=10, null=True)
     motor = models.CharField(max_length=10, null=True)
     marca = models.CharField(max_length=100, default='Marca X', null=True)
+    fecha_compra = models.DateTimeField(auto_now_add=True, null=True)
 
     def __unicode__(self):
         return self.placa
