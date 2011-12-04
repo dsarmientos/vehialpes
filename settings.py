eefe0b3856c77810995cc2c89d0c2becba4114ed
Daniel Sarmiento <dsarmientos@gmail.com>
2011-12-03 20:06:43 -0500

    Custom template for admin

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 # Django settings for vehialpes project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -102,11 +104,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'vehialpes.urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
